f9b5562a1d423780e6c1299a18ac4f18c9645444
yonicb <yonicb20@gmail.com>
2020-01-12 17:08:23 -0500

    perf: ⚡️ Buil Main Files (style and scripts)

--- dist/cbscroll-top.d.ts
+++
@@ -1,7 +0,0 @@
-import './index.scss';
-import { ScrollTopInit } from 'type';
-declare class ScrollTop implements ScrollTopInit {
-    d: Document;
-    createElement(): void;
-}
-export default ScrollTop;
